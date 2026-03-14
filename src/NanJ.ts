a78ab3b946b816ef826f17b05a1bae85d9ca07c9
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2026-03-14 16:47:04 +0900

    NanJ

---
+++ src/NanJ.ts
@@ -0,0 +1,37 @@
+import "./NanJ.scss";
+
+export type NanJState = "normal" | "happy" | "angry";
+
+export default class NanJ {
+  el: HTMLElement;
+  _state: NanJState = "normal";
+  onFadeOut?: (nanJ: NanJ) => void;
+
+  constructor() {
+    this.el = document.createElement("div") as HTMLElement;
+    this.el.classList.add("NanJ");
+
+    const span = document.createElement("span") as HTMLElement;
+    this.el.appendChild(span);
+
+    this.el.classList.add("--" + this._state);
+
+    this.el.addEventListener("animationend", () => {
+      if (this._state == "happy") {
+        this.onFadeOut?.(this);
+      }
+    });
+  }
+
+  set state(state: NanJState) {
+    this._state = state;
+    this.el.classList.remove("--normal");
+    this.el.classList.remove("--angry");
+    this.el.classList.remove("--happy");
+    this.el.classList.add("--" + state);
+  }
+
+  get state() {
+    return this._state;
+  }
+}
