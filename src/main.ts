d94cb7b468bf0ca74194333cf140d160cdd47529
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2026-04-05 00:34:39 +0900

    Delete the files from the current version (excluding assets) in preparation for the rewrite in Rust.

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-import MainViewController from "./MainViewController";
-
-const mainViewController = new MainViewController();
-document.body.appendChild(mainViewController.el);
