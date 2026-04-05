1155172ab1e8a5cd1965cd1fb5aab2eac58dce3e
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2026-04-06 00:16:35 +0900

    Implement random selection logic for question

--- src/question.rs
+++ src/question.rs
@@ -1,5 +1,7 @@
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
+use rand::seq::SliceRandom;
+use rand::rng;
 use wasm_bindgen::prelude::*;
 use crate::task_queue::TaskQueue;
 
@@ -23,15 +25,28 @@ pub trait QuestionObserver {
 pub struct Question {
     observers: Vec<Weak<RefCell<dyn QuestionObserver>>>,
     task_queue: Rc<RefCell<TaskQueue>>,
+    current_item: QuestionItem,
+    items: Vec<QuestionItem>,
+    lot_items: Vec<QuestionItem>,
+    accidental: bool,
 }
 
 impl Question {
     pub fn new(task_queue: Rc<RefCell<TaskQueue>>) -> Self {
         let observers: Vec<Weak<RefCell<dyn QuestionObserver>>> = Vec::new();
+        let items = build_question_items();
+        let mut lot_items: Vec<QuestionItem> = items.iter().filter(|&i| !i.note.contains("#")).cloned().collect();
+
+        lot_items.shuffle(&mut rng());
+        let current_item = lot_items.pop().expect("definitely exists");
 
         Self {
             observers,
             task_queue,
+            items,
+            lot_items,
+            current_item,
+            accidental: false,
         }
     }
 
@@ -55,14 +70,75 @@ impl Question {
     }
 
     pub fn start(&mut self) {
-        self.notify_event(QuestionEvent::New(QuestionItem{
-            string: 6,
-            fret: 12,
-            note: "C",
-        }))
+        self.notify_event(QuestionEvent::New(self.current_item));
     }
 
     pub fn answer(&mut self, note: &str) {
-        self.notify_event(QuestionEvent::Answer{ correct: true })
+        let correct = self.current_item.note == note;
+
+        self.notify_event(QuestionEvent::Answer{ correct });
+
+        if correct {
+            if self.lot_items.is_empty() {
+                self.lot_items = self.items.iter().filter(|i| {
+                    if self.accidental {
+                        true
+                    } else {
+                        !i.note.contains("#")
+                    }
+                }).cloned().collect();
+
+                self.lot_items.shuffle(&mut rng());
+            }
+
+            self.current_item = self.lot_items.pop().expect("definitely exists");
+            self.notify_event(QuestionEvent::New(self.current_item));
+        }
     }
+}
+
+const NOTE_LIST: [&str; 12] = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
+
+fn build_question_items() -> Vec<QuestionItem> {
+    let mut items: Vec<QuestionItem> = Vec::new();
+
+    for i in 1..=6 {
+        let mut note_index: usize = match i {
+            1 => NOTE_LIST.iter().position(|&p| p == "E" ).expect("definitely exists"),
+            2 => NOTE_LIST.iter().position(|&p| p == "B" ).expect("definitely exists"),
+            3 => NOTE_LIST.iter().position(|&p| p == "G" ).expect("definitely exists"),
+            4 => NOTE_LIST.iter().position(|&p| p == "D" ).expect("definitely exists"),
+            5 => NOTE_LIST.iter().position(|&p| p == "A" ).expect("definitely exists"),
+            6 => NOTE_LIST.iter().position(|&p| p == "E" ).expect("definitely exists"),
+            _ => 0,
+        };
+
+        for j in 0..=12 {
+            items.push(QuestionItem{
+                string: i,
+                fret: j,
+                note: NOTE_LIST[note_index],
+            });
+
+            note_index += 1;
+            if NOTE_LIST.len() <= note_index {
+                note_index = 0;
+            }
+        }
+    }
+
+    items
 }
