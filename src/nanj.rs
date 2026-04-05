4507504f1170cb093c1ca195c05b8e0e18f197e1
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2026-04-05 17:03:48 +0900

    Drawing NanJ

--- src/nanj.rs
+++ src/nanj.rs
@@ -27,12 +27,27 @@ impl View for NanJ {
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &CanvasRenderingContext2d, _: f64, next: &mut bool) -> Result<(), JsValue> {
+    fn draw(&mut self, ctx: &CanvasRenderingContext2d, dpr: f64, next: &mut bool) -> Result<(), JsValue> {
         let elapsed = self.performance.now() - self.started_at;
 
         let y = elapsed.min(1000.0) * 0.1;
 
+        ctx.set_stroke_style_str("lightgray");
+        ctx.set_fill_style_str("#ddddf0");
+
+        ctx.set_line_width(2.0 * dpr);
+
+        ctx.begin_path();
+
+        ctx.arc(self.frame.width / 2., self.frame.height / 2. + y, self.frame.width / 2., 0., std::f64::consts::PI * 2.)?;
+        ctx.fill();
+
+        ctx.clip();
+
         ctx.draw_image_with_html_canvas_element_and_dw_and_dh(&self.source_canvas, 0.0, y, self.frame.width, self.frame.height)?;
+
+        ctx.arc(self.frame.width / 2., self.frame.height / 2. + y, self.frame.width / 2. - 1. * dpr, 0., std::f64::consts::PI * 2.)?;
+        ctx.stroke();
 
         if 1000.0 > elapsed {
             *next = true
