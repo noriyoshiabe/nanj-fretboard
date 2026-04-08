616edeca773b3180ec23640753cc5f0fba35b792
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2026-04-09 02:50:16 +0900

    Add query hash of wasm to bridge js in case only wasm file has changed

--- build.sh
+++ build.sh
@@ -18,5 +18,5 @@ cp index.html dist/
 HASH_JS=`sha1sum dist/pkg/nanj_fretboard.js | awk '{print $1}'`
 HASH_WASM=`sha1sum dist/pkg/nanj_fretboard_bg.wasm | awk '{print $1}'`
 
-sed -i "" "s/nanj_fretboard.js/nanj_fretboard.js?h=${HASH_JS}/g" dist/index.html
+sed -i "" "s/nanj_fretboard.js/nanj_fretboard.js?h=${HASH_JS}-${HASH_WASM}/g" dist/index.html
 sed -i "" "s/nanj_fretboard_bg.wasm/nanj_fretboard_bg.wasm?h=${HASH_WASM}/g" dist/pkg/nanj_fretboard.js
